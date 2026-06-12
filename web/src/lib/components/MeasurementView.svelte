22894cb5a9da72e7a45729a4f5f7a5436167dc6c
John Elizarraras <elizarrj27@gmail.com>
2026-06-12 16:03:26 -0500

    fix gitignore

---
+++ web/src/lib/components/MeasurementView.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    let {open, measurementId} = $props();
+	// let open = $state(false);
+	// let measurementId = $state('');
+
+	function close() {
+		open = false;
+	}
+</script>
+
+{#if open}
+	<dialog open>
+		<article>
+			<header>
+				<button
+					aria-label="Close"
+					rel="prev"
+					onclick={close}
+				></button>
+
+				<p>
+					<strong>EMG Measurement {measurementId}</strong>
+				</p>
+			</header>
+
+			<p>
+				This is dummy test data for the EMG dashboard.
+			</p>
+
+			<ul>
+				<li>Muscle: Biceps Brachii</li>
+				<li>Timestamp: 2026-06-05 09:14</li>
+				<li>Strength (RMS μV): 84.2</li>
+				<li>Dominant Frequency: 72 Hz</li>
+				<li>Signal Quality: 98%</li>
+				<li>Fatigue Index: 0.12</li>
+				<li>Status: Normal</li>
+			</ul>
+		</article>
+	</dialog>
+{/if}
